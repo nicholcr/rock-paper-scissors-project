3395b0b727be8110c8dbe21ca5301e96d5b8712c
nicholcr <73195431+nicholcr@users.noreply.github.com>
2025-06-22 02:33:26 -0500

    add logic to play a round of rock paper scissors

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,17 +1,35 @@
-const computerSelection = 0;
-const humanSelection = 0;
+const computerSelection = getComputerChoice();
+const humanSelection = getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    let computerChoice = ["Rock", "Paper", "Scissors"];
+    let computerChoice = ["rock", "paper", "scissors"];
     let index = Math.floor(Math.random() * 3);
     return computerChoice[index];
 }
 
 function getHumanChoice() {
     let userChoice = prompt("Please type 'Rock', 'Paper', or 'Scissors'");
-    return userChoice;
+    return userChoice.toLowerCase();
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "You both chose the same move!"
+    } else if (humanChoice === "rock" && computerChoice === "scissors" 
+        || humanChoice === "scissors" && computerChoice === "paper" 
+        || humanChoice === "paper" && computerChoice === "rock") {
+            return "You win!"
+            humanScore++;
+    } else {
+        return "You lose..."
+        computerScore++;
+    }
+}
 
+function playGame() {
+    
+}
 
-console.log(humanSelection);
+console.log(playRound(humanSelection, computerSelection));
