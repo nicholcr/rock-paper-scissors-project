3b86ed8c7b67bc075bee78303679f46f76596336
nicholcr <73195431+nicholcr@users.noreply.github.com>
2025-07-15 20:22:55 -0500

    added UI to the project

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,56 @@
 let humanScore = 0;
 let computerScore = 0;
+let humanSelection = "";
+let computerSelection = "";
 
+const finalResult = function () {
+    if (humanScore === computerScore) {
+        return "You tied";
+    } else if (humanScore > computerScore) {
+        return "You win";
+    } else {
+        return "You lose";
+    }
+}
+
+const btnRock = document.querySelector("#btnRock");
+btnRock.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+    displayResults();
+});
+
+const btnPaper = document.querySelector("#btnPaper");
+btnPaper.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+    displayResults();
+});
+
+const btnScissors = document.querySelector("#btnScissors");
+btnScissors.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+    displayResults();
+});
+
+const gameResults = document.querySelector("#gameResults");
+
+function displayResults() {
+    if (humanScore >= 5 || computerScore >= 5) {
+        const gameOverMessage = document.createElement("div");
+        gameOverMessage.textContent = finalResult() + " - final score User: " + humanScore + ", Computer: " + computerScore;
+
+        gameResults.appendChild(gameOverMessage);
+    } else {
+        const updatedChoices = document.createElement("div");
+        updatedChoices.textContent = "human choice: " + humanSelection + " computer choice: " + computerSelection;
+
+        const updatedScores = document.createElement("div");
+        updatedScores.textContent = "human score: " + humanScore + " computer score: " + computerScore;
+        updatedScores.style.padding = "10px";
+
+        gameResults.appendChild(updatedChoices);
+        gameResults.appendChild(updatedScores);
+    }
+}
 
 function getComputerChoice() {
     let computerChoice = ["rock", "paper", "scissors"];
@@ -8,12 +58,10 @@ function getComputerChoice() {
     return computerChoice[index];
 }
 
-function getHumanChoice() {
-    let userChoice = prompt("Please type 'Rock', 'Paper', or 'Scissors'");
-    return userChoice.toLowerCase();
-}
-
 function playRound(humanChoice, computerChoice) {
+    humanSelection = humanChoice;
+    computerSelection = computerChoice;
+
     if (humanChoice === computerChoice) {
         // do nothing
     } else if (humanChoice === "rock" && computerChoice === "scissors"
@@ -23,28 +71,9 @@ function playRound(humanChoice, computerChoice) {
     } else {
         computerScore++;
     }
+
+    console.log("human choice: " + humanChoice + " computer choice: " + computerChoice);
+    console.log("human score: " + humanScore + " computer score: " + computerScore);
 }
 
-function playGame() {
-    for (let i = 1; i < 6; i++) {
-        const computerSelection = getComputerChoice();
-        const humanSelection = getHumanChoice();
-
-        playRound(humanSelection, computerSelection);
-    }
-
-    const gameResult = function () {
-        if (humanScore === computerScore) {
-            return "You tied";
-        } else if (humanScore > computerScore) {
-            return "You win";
-        } else {
-            return "You lose";
-        }
-    }
-
-    const gameOverMessage = gameResult() + " - final score User: " + humanScore + ", Computer: " + computerScore;
-    return gameOverMessage;
-}
-
-console.log(playGame());
+// console.log(playGame());
